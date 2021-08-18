2a93817614983ec2d1a5fc4d6c5bb133e1f2e79c
mhmadam <86463521+mhmadam@users.noreply.github.com>
2021-08-18 17:49:17 +0430

    T7 Created

---
+++ T7/tut7/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def home(request):
+	context = {
+		'posts':Post.objects.all(),
+		'title':'Home'
+	}
+	return render(request, 'blog/home.html', context)
+def about(request):
+	return render(request, 'blog/about.html',{'content':'About Blog', 'title':'About'})
+	
