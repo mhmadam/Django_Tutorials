e21a511b86e789396f4820932adb2581027ec46a
mhmadam <86463521+mhmadam@users.noreply.github.com>
2021-08-18 20:38:40 +0430

    T8 created, T7 fixed.

--- T8/tut8/users/views.py
+++ T8/tut8/users/views.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
